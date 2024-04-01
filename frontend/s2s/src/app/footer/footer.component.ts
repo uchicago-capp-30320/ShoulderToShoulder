fa0448c27a4c381bd660038f0e55a01148d52024
Chanteria Milner <chanteria.milner@gmail.com>
2024-04-01 18:17:54 -0500

    adding summary to footer component docstrings

--- frontend/s2s/src/app/footer/footer.component.ts
+++ frontend/s2s/src/app/footer/footer.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
  * The footer contains the name of the application, social media links,
  * logo attributions, and quick links to other parts of the application.
  * 
+ * @summary Footer component class.
  */
 @Component({
   selector: 'app-footer',
