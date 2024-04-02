0984464d93c28e7819f1291ef1c4c4b865522037
Chanteria Milner <chanteria.milner@gmail.com>
2024-04-02 14:58:00 -0500

    setup authorization guards

--- frontend/s2s/src/app/app-routing.module.ts
+++ frontend/s2s/src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// guards
+// import { AuthGuard } from './guards/auth.guard';
+// import { AdminGuard } from './guards/admin.guard';
+
 // Components
 import { HomepageComponent } from './homepage/homepage.component';
 // import { LoginComponent } from './login/login.component';
