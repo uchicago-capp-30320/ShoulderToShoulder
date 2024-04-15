43531ced629eda6b351298ad711d8d09c877ff4b
Chanteria Milner <chanteria.milner@gmail.com>
2024-04-15 16:22:56 -0500

    implemented preferences survey tests

--- frontend/s2s/src/app/signup-page/signup-page.component.spec.ts
+++ frontend/s2s/src/app/signup-page/signup-page.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignupPageComponent } from './signup-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
