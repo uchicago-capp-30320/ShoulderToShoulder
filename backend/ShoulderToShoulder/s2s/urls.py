4ff83acf9e65d5f4f949d7044930eaf323b4b92e
necabotheking <aichacamara@uchicago.edu>
2024-04-17 13:30:00 -0500

    Create Urls

--- backend/ShoulderToShoulder/s2s/urls.py
+++ backend/ShoulderToShoulder/s2s/urls.py
@@ -5,6 +5,13 @@ from . import views
 
 router = DefaultRouter()
 router.register(r"hobbies", views.HobbyViewSet)
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
+router.register(r"events", views.EventViewSet)
+router.register(r"calendar", views.CalendarViewSet)
+router.register(r"onboarding", views.OnbordingViewSet)
+router.register(r"availability", views.AvailabilityViewSet)
+router.register(r"scenarios", views.ScenariosiewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
