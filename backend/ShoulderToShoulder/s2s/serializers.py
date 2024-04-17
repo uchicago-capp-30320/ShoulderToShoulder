e3c766f7703d2254854327aecdbb5a28d3e69067
necabotheking <aichacamara@uchicago.edu>
2024-04-17 13:29:44 -0500

    Create Serializers

--- backend/ShoulderToShoulder/s2s/serializers.py
+++ backend/ShoulderToShoulder/s2s/serializers.py
@@ -6,3 +6,52 @@ class HobbySerializer(ModelSerializer):
     class Meta:
         model = Hobby
         fields = "__all__"
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
+class GroupSerializer(ModelSerializer):
+    class Meta:
+        model = Group
+        fields = "__all__"
+        
+        
+class UserAdminSerializer(ModelSerializer):
+    class Meta:
+        model: UserAdmin
+        fields = "__all__"
+        
+        
+class EventSerializer(ModelSerializer):
+    class Meta:
+        model = Event
+        fields = "__all__"
+        
+        
+class CalendarSerializer(ModelSerializer):
+    class Meta:
+        model = Calendar
+        fields = "__all__"
+
+
+class OnbordingSerializer:
+    class Meta:
+        model = Onboarding
+        fields = "__all__"
+
+
+class ScenariosSerializer:   
+    class Meta:
+        model = Scenario
+        fields = "__all__"    
+       
+       
+        
+class AvialabilitySerializer:
+    class Meta:
+        model = Availability
+        fields = "__all__"
