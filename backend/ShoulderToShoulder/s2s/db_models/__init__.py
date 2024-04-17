13642db4dc966382bb420796be4d65b3186f3048
necabotheking <aichacamara@uchicago.edu>
2024-04-17 13:29:50 -0500

    Create ViewSets

--- backend/ShoulderToShoulder/s2s/db_models/__init__.py
+++ backend/ShoulderToShoulder/s2s/db_models/__init__.py
@@ -4,3 +4,6 @@ from s2s.db_models.event import Event
 from s2s.db_models.availability import Availability
 from s2s.db_models.user import User
 from s2s.db_models.group import Group
+from s2s.db_models.useradmin import UserAdmin
+from s2s.db_models.onboarding import Onboarding
+from s2s.db_models.scenarios import Scenarios
--- backend/ShoulderToShoulder/s2s/views.py
+++ backend/ShoulderToShoulder/s2s/views.py
@@ -15,3 +15,38 @@ class HobbyViewSet(viewsets.ModelViewSet):
     queryset = Hobby.objects.all()
     serializer_class = HobbySerializer
     permission_classes = [permissions.IsAuthenticated]
+    
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+class CalendarViewSet(viewsets.ModelViewSet):
+    queryset = Calendar.objects.all()
+    serializer_class = CalendarSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+class OnbordingViewSet(viewsets.ModelViewSet):
+    queryset = Onboarding.objects.all()
+    serializer_class = OnbordingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+class AvailabilityViewSet(viewsets.ModelViewSet):
+    queryset = Availability.objects.all()
+    serializer_class = AvialabilitySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class ScenariosiewSet(viewsets.ModelViewSet):
+    queryset = Scenarios.objects.all()
+    serializer_class = ScenariosSerializer
+    permission_classes = [permissions.IsAuthenticated]    
