5d7141b0b3b30dd5904f701e5a73b3c6c08b9d94
Chanteria Milner <chanteria.milner@gmail.com>
2024-04-17 22:15:43 -0500

    fixing permissions

--- backend/ShoulderToShoulder/ShoulderToShoulder/settings.py
+++ backend/ShoulderToShoulder/ShoulderToShoulder/settings.py
@@ -102,6 +102,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
 }
 
