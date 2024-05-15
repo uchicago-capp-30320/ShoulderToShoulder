e20c2e1d0f09afcc10b0eb147f94f76c90a8cb1f
Chanteria Milner <chanteria.milner@gmail.com>
2024-05-15 14:31:00 -0500

    update production frontend/backend endpoints

--- frontend/s2s/src/environments/environment.prod.ts
+++ frontend/s2s/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export var environment = {
     production: true,
-    api: 'http://localhost:8000/api',
-    s2s: 'http://localhost:4200',
+    api: 'https://admin.shouldertoshoulder.me/api',
+    s2s: 'https://shouldertoshoulder.me',
     appToken: 'vGYjtU_0M6uT8UUnBh5BOBxADIM5XWd8NZXwpiZAd9A'
 }
